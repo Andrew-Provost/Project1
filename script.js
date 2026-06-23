49bb23748bc004714aca38557d8163cc78b271f1
Andrew-Provost <andrew.provost1@gmail.com>
2026-06-22 19:04:17 -0500

    console logs

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("JavaScript is connected");
+let ticker = "ES";
+let price = 6000;
+console.log(ticker);
+console.log(price);
+price = 6010;
+console.log(price);
+const siteName = "Futures Market Pulse";
+console.log(siteName);
