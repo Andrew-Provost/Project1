d368218dc4e8eeb4325df1923e3ed46fdc039d9e
Andrew-Provost <andrew.provost1@gmail.com>
2026-06-23 10:51:40 -0500

    feat: add basic arithmetic functions

--- script.js
+++ script.js
@@ -7,3 +7,49 @@ price = 6010;
 console.log(price);
 const siteName = "Futures Market Pulse";
 console.log(siteName);
+const tickerSymbol ="NQ";
+let currentPrice = 20000;
+const marketOpen = true;
+console.log(typeof tickerSymbol);
+console.log(typeof currentPrice);
+console.log(typeof marketOpen);
+const entryPrice = 6000;
+const exitPrice = 6010;
+const pointsGained = exitPrice - entryPrice;
+console.log(pointsGained);
+function showWelcomeMessage() {
+    console.log("Welcome to the Futures Market Pulse");
+}
+showWelcomeMessage();
+function showTicker(tickerSymbol) {
+    console.log(tickerSymbol);
+}
+showTicker("ES");
+showTicker("NQ");
+function calculatePoints(entryPrice, exitPrice) {
+    const pointsGained = exitPrice - entryPrice;
+    return pointsGained;
+}
+const tradeResult= calculatePoints(6000, 6010);
+console.log(tradeResult);
+if (tradeResult > 0) {
+    console.log("Winning trade");
+} else {
+    console.log("Losing trade");
+}
+function addNumbers (a, b) {
+    return a + b;
+}
+console.log(addNumbers(1, 2));
+function subtractNumbers (a, b) {
+    return a - b;
+}
+console.log(subtractNumbers(2, 1));
+function multiplyNumbers (a, b) {
+    return a * b;
+}
+console.log(multiplyNumbers(2, 3));
+function divideNumbers (a, b) {
+    return a / b;
+}
+console.log(divideNumbers(6, 3));
